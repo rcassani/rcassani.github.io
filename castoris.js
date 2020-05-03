f874153573e40ce2a2339214022d4b9ad6530ce5
Raymundo Cassani <raymundo.cassani@gmail.com>
2020-05-02 23:46:07 -0400

    js email, subtitle, aesthetics

---
+++ castoris.js
@@ -0,0 +1,14 @@
+// script for email
+var m;
+m = 'r';
+m+= 'aym';
+m+= 'u';
+m+= 'ndo.c';
+m+= 'as';
+m+= 'san';
+m+= 'i@g';
+m+= 'mai';
+m+= 'l.c';
+m+= 'om';
+$x = document.getElementsByClassName("fas fa-envelope fa-lg")[0].parentNode;
+$x.href = 'mailto:' + m;
